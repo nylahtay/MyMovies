38da05f378c3913b48879983dabba2cacb676c0b
nylahtay <nylahtayrogers@gmail.com>
2021-05-19 05:03:40 -0500

    #Splash - added splash screen

---
+++ app/src/main/java/com/example/mymovies/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.example.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    Animation bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        //set anim
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_up);
+        //set image
+        final ImageView image1 = findViewById(R.id.titleImg);
+
+        //link animation to imageview
+        image1.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
